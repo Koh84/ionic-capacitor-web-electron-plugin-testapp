c0ecbee3817d3f6b3035cf079a55c3d611e17b09
Kelvin Koh <kelvin.koh@prometheanworld.com>
2022-12-13 15:19:53 +0000

    added electron test but electron throwing error when using web plugin, and electron plugin is not supported in electron community V4, web test app is working with the web plugin

---
+++ electron/src/rt/electron-plugins.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const PrometheanPlugin = require('..\\..\\..\\node_modules\\promethean-plugin\\electron\\dist\\plugin.js');
+
+module.exports = {
+  PrometheanPlugin,
+}
